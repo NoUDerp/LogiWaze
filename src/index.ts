88cdb4cb27e9538f4cf707c888772ae7da563358
Derp <Derp>
2025-01-19 11:34:18 -0800

    Async

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,46 @@
 //@ts-nocheck
 'use strict';
 
-module.exports.L = require('leaflet');
+export {default as L} from 'leaflet';
+export {default as $} from 'jquery';
 
-module.exports.$ = require('jquery');
+import * as IVectorControlGrid from './IVectorControlGrid';
+import * as IVectorTextGrid from './IVectorTextGrid';
+import * as IRouter from './IRouter';
+import { SAPI } from './API';
+import * as Geocoder from './IGeocoder';
+import * as IPanel from './Panel';
 
-module.exports.VectorControlGrid = {
-    Create: (MaxNativeZoom, MaxZoom, Offset, API, RoadWidth, ControlWidth, GridDepth) => require('./IVectorControlGrid.js').Create(MaxNativeZoom, MaxZoom, Offset, API, RoadWidth, ControlWidth, GridDepth)
-};
+export class VectorControlGrid {
+    Create(MaxNativeZoom, MaxZoom, Offset, API, RoadWidth, ControlWidth, GridDepth) {
+        return IVectorControlGrid.Create(MaxNativeZoom, MaxZoom, Offset, API, RoadWidth, ControlWidth, GridDepth);
+    }
+}
 
-module.exports.VectorTextGrid = {
-    Create: (MaxZoom, Offset) => require('./IVectorTextGrid.ts').Create(MaxZoom, Offset)
-};
+export class VectorTextGrid {
+    Create(MaxZoom, Offset) {
+        return IVectorTextGrid.Create(MaxZoom, Offset);
+    }
+}
 
-module.exports.FoxholeRouter = {
-    Create: (mymap, API) => require('./IRouter.ts').Create(mymap, API)
-};
+export class FoxholeRouter {
+    Create(mymap, API) {
+        return IRouter.Create(mymap, API);
+    }
+}
 
-module.exports.API = {
-    Create: () => require('./API.ts').API
-};
+export class API {
+    Create() {
+        return SAPI.Create();
+    }
+}
 
-module.exports.FoxholeGeocoder = {
-    Create: (API) => require('./IGeocoder.ts').FoxholeGeocoder(API)
-};
+export class FoxholeGeocoder {
+    Create(API){ return Geocoder.FoxholeGeocoder(API); }
+}
 
-module.exports.Panel = {
-    Create: (APIManager, Router, Geocoder) => require('./Panel.ts').Create(APIManager, Router, Geocoder)
+export class Panel {
+    Create(APIManager, Router, Geocoder) { return IPanel.Create(APIManager, Router, Geocoder); }
 }
 
 module.exports.Shards = require('../Shards.json');
