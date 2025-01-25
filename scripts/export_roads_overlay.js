95e15d1ebcf49a13d578be7eebd7c91487011b46
Derp <Derp>
2025-01-25 03:36:16 -0800

    Async

--- scripts/export_roads_overlay.js
+++ scripts/export_roads_overlay.js
@@ -1,12 +1,11 @@
 import sharp from 'sharp';
-import fs from "fs";
 
 try {
     const args = process.argv.slice(2);
-    const svg = await sharp(args[0]).resize(11264, 12432).toBuffer({ resolveWithObject: true });
-    const map = await sharp(args[1]) .toBuffer({ resolveWithObject: true });
-    await map.composite([svg]).png().toFile(args[2]);
-    console.log(info);
+    const map = await sharp(args[1]).toBuffer({resolveWithObject: true});
+    const {data} = await sharp(args[0]).resize( map.info.width, map.info.height).toBuffer({resolveWithObject: true}); // 11264, 12432 
+    await sharp(map.data).composite([{input:data}]).png().toFile(args[2]);
 } catch (err) {
     console.log(err)
+
 }
