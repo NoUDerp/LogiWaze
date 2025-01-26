b1e8b422b94f987754fc3fc352ee7ce4206b9a46
Derp <Derp>
2025-01-26 01:26:42 -0800

    Async

--- scripts/roads.js
+++ scripts/roads.js
@@ -12,6 +12,8 @@ for (let i = features.length - 1; i >= 0; i--)
 
 console.log("Updating road scaling");
 
+const newFeatures = [];
+
 for (const j of features) {
     // update roads set tier=3 where tier=4; 
     // update roads set tier=1 where tier=0; 
@@ -22,12 +24,28 @@ for (const j of features) {
 
     // update [roads] set geometry = ATM_Transform(ATM_Transform(geometry, ATM_CreateScale(128.0 / 20037500, 128.0 / 20037500)), ATM_CreateTranslate(128.0, -128.0)); 
     // #ogr2ogr Roads.geojson "$roads" -f GeoJSON -nlt LINESTRING -explodecollections -lco COORDINATE_PRECISION=3 roads
-    for (let i = 0; i < j.geometry.coordinates.length; i++)
-        for (let k = 0; k < j.geometry.coordinates[i].length; k++) {
-            const v = j.geometry.coordinates[i][k];
+
+    // convert to line string
+    if (j.geometry.type === "MultiLineString")
+        for (let i = 0; i < j.geometry.coordinates.length; i++)
+            newFeatures.push({
+                properties: j.properties,
+                type: "Feature",
+                geometry: {type: "LineString", coordinates: j.geometry.coordinates[i]}
+            });
+    else
+        newFeatures.push(j);
+}
+
+for (const j of newFeatures) {
+    for (let i = 0; i < j.geometry.coordinates.length; i++){
+            const v = j.geometry.coordinates[i];
             v[0] = Math.round((parseFloat(v[0]) * 128 / 20037500 + 128) * 1000) / 1000;
             v[1] = Math.round((parseFloat(v[1]) * 128 / 20037500 - 128) * 1000) / 1000;
         }
+
 }
+
+geojson.features = newFeatures;
 
 fs.writeFileSync('Roads.json', JSON.stringify(geojson, null, 0));
