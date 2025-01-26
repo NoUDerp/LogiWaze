b00150d1fc55bb28f6d077c3dd63a2f969fe360d
Derp <Derp>
2025-01-26 00:27:05 -0800

    Async

--- scripts/roads.js
+++ scripts/roads.js
@@ -1,66 +1,33 @@
-import * as spl from 'spl';
 import * as fs from 'fs';
-import * as gdal from 'gdal-async';
 
-console.log('deleting temp.sqlite');
-fs.rm('temp.sqlite', {}, null);
+const geojson = JSON.parse(fs.readFileSync('road_source.geojson', 'utf-8'));
 
-console.log('converting road_source.geojson to spatialite (for manipulation)');
-const dataset = await gdal.openAsync('../road_source.geojson');
-const layer = dataset.layers.get(0);
+console.log('Deleting empty geometry');
+// #echo "delete from [roads] where [geometry] is null; 
 
-const options = [
-    '-f', 'SQLite', // Output format
-    '-lco', 'ENCODING=UTF-8' // Optional: Set encoding
-];
+const features = geojson.features;
+for (let i = features.length - 1; i >= 0; i--)
+    if (features[i].geometry === undefined || features[i].geometry === "" || features[i].geometry.coordinates === undefined || features[i].geometry.coordinates.length === 0)
+        features.splice(i, 1);
 
-await gdal.vectorTranslateAsync('temp.sqlite', layer,  options);
+console.log("Updating road scaling");
 
-console.log('opening temp.sqlite');
-const db = await spl.open('temp.sqlite'); // Open or create a SpatiaLite database
+for (const j of features) {
+    // update roads set tier=3 where tier=4; 
+    // update roads set tier=1 where tier=0; 
+    // update roads set tier=(3-tier)+1;" \
+    if (j.properties.tier === 4) j.properties.tier = 3;
+    if (j.properties.tier === 0) j.properties.tier = 1;
+    j.properties.tier = (3 - j.properties.tier) + 1;
 
-// async function main() {
-//     await db.exec('SELECT InitSpatialMetadata()'); // Initialize SpatiaLite
-//
-//     // Execute spatial queries
-//     const result = await db.exec('SELECT AsText(MakePoint(1, 2))');
-//     console.log(result[0].values[0][0]); // Output: POINT(1 2)
-//
-//     await db.close();
-// }
+    // update [roads] set geometry = ATM_Transform(ATM_Transform(geometry, ATM_CreateScale(128.0 / 20037500, 128.0 / 20037500)), ATM_CreateTranslate(128.0, -128.0)); 
+    // #ogr2ogr Roads.geojson "$roads" -f GeoJSON -nlt LINESTRING -explodecollections -lco COORDINATE_PRECISION=3 roads
+    for (let i = 0; i < j.geometry.coordinates.length; i++)
+        for (let k = 0; k < j.geometry.coordinates[i].length; k++) {
+            const v = j.geometry.coordinates[i][k];
+            v[0] = Math.round((parseFloat(v[0]) * 128 / 20037500 + 128) * 1000) / 1000;
+            v[1] = Math.round((parseFloat(v[1]) * 128 / 20037500 - 128) * 1000) / 1000;
+        }
+}
 
-// #echo "Starting build:"
-//
-// ## Set our build revision for the cache
-// #sed -i "s/var build.*/var build=\"$(git rev-parse HEAD)\";/" index.html;
-// #
-// #echo "Generating road data:"
-// #
-// ## Run road generation
-// #roads="$(mktemp).sqlite";
-// #ogr2ogr "$roads" ./road_source.geojson -f SQLite -dsco SPATIALITE=YES -nln roads
-// #
-// #echo "delete from [roads] where [geometry] is null; update [roads] set geometry = ATM_Transform(ATM_Transform(geometry, ATM_CreateScale(128.0 / 20037500, 128.0 / 20037500)), ATM_CreateTranslate(128.0, -128.0)); update roads set tier=3 where tier=4; update roads set tier=1 where tier=0; update roads set tier=(3-tier)+1;" \
-// #        | spatialite "$roads"
-// #
-// #rm -rf Roads.geojson
-// #ogr2ogr Roads.geojson "$roads" -f GeoJSON -nlt LINESTRING -explodecollections -lco COORDINATE_PRECISION=3 roads
-// #rm "$roads"
-// #
-// #node export_roads_svg.js > Roads.svg
-// #
-// #echo "Packing web assets:"
-// #
-// ## Pack our js
-// #webpack --config webpack.config.js --mode production -o ./ .
-// #
-// ## Notate scripts with build revision for cache purposes
-// #sed -i "s/src=\"FoxholeRouter.js[^\"]*\"/src=\"FoxholeRouter.js?$(md5sum FoxholeRouter.js | cut -d " " -f1)\"/g" index.html
-// #sed -i "s/\"ServiceWorker.js[^\"]*\"/\"ServiceWorker.js?$(md5sum ServiceWorker.js | cut -d " " -f1)\"/g" index.html
-// #
-// #echo "Finished!"
-//
-//
-// npx dotnet run --project MapStitcher MapStitcher/map.xml map.png && npx rimraf Tiles && npx mkdirp Tiles && npx dotnet run --project Tiler -input map.png -zoom 7 -filename "Tiles/{z}_{x}_{y}.webp"
-//
-// parcel build
+fs.writeFileSync('Roads.json', JSON.stringify(geojson, null, 0));
