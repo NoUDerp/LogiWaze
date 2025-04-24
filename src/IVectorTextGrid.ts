fae31dbd871ad1d2f8fbaaa9fef8ee014562650f
Derp <Derp>
2025-04-23 21:50:39 -0700

    Class upgrade

--- src/IVectorTextGrid.ts
+++ src/IVectorTextGrid.ts
@@ -1,6 +1,6 @@
 //@ts-nocheck
-let L = require('leaflet');
-let intersects = require('intersects');
+import L from 'leaflet';
+import intersects from 'intersects';
 
 function controlToFont(control, ctx, boring_font) {
     if (boring_font) {
@@ -35,41 +35,40 @@ function controlToFont(control, ctx, boring_font) {
         }
 };
 
-var VectorGridPrototype = L.GridLayer.extend({
-    zoomScale: function (zoom) {
+class VectorGridPrototype extends L.GridLayer {
+    zoomScale(zoom) {
         return .65 * (1 + this.max_zoom - zoom);
-    },
-    shadowSize: 20,
-    draw: true,
-    boring: true,
-    pixelScale: window.devicePixelRatio,
-    recalculateSizes: function () {
-        var canvas = L.DomUtil.create('canvas', 'leaflet-tile');
+    }
+
+    shadowSize: number = 20
+    draw: boolean = true
+    boring: boolean = true
+    pixelScale: number = window.devicePixelRatio
+
+    recalculateSizes() {
+        const canvas = L.DomUtil.create('canvas', 'leaflet-tile');
         ctx = canvas.getContext('2d');
         for (let k of this.sources) {
             controlToFont(k.control, ctx, this.boring);
-            var size = ctx.measureText(this.boring ? k.original_text : k.text);
+            const size = ctx.measureText(this.boring ? k.original_text : k.text);
             k.size = {
                 width: (size.actualBoundingBoxRight - size.actualBoundingBoxLeft) / this.grid_x_size,
                 height: (size.actualBoundingBoxAscent + size.actualBoundingBoxDescent) / this.grid_y_size
             };
         }
-    },
+    }
 
-    createTile: function (coords, done) {
-
-        var raw_scale = this.zoomScale(coords.z);
-
-        var hd_ratio = this.pixelScale;
-        var size = this.getTileSize();
-        var tile = null;
+    createTile(coords, done) {
+        const raw_scale = this.zoomScale(coords.z);
+        const hd_ratio = this.pixelScale;
+        const size = this.getTileSize();
+        let tile = null;
         tile = L.DomUtil.create('canvas', 'leaflet-tile logiwaze-text');
         tile.crossorigin = "Anonymous";
         tile.setAttribute("crossorigin", "Anonymous");
 
         tile.width = size.x * hd_ratio;
         tile.height = size.y * hd_ratio;
-
         tile.style.width = (size.x * hd_ratio).toString().concat("px");
         tile.style.height = (size.y * hd_ratio).toString().concat("px");
 
@@ -86,7 +85,7 @@ var VectorGridPrototype = L.GridLayer.extend({
         let shadowSize = this.shadowSize;
 
         function draw(i, boring) {
-            var startTime = Date.now();
+            const startTime = Date.now();
             for (; i < sources.length; i++) {
                 let j = sources[i];
                 if (coords.z >= j.zoomMin && coords.z < j.zoomMax) {
@@ -130,59 +129,59 @@ var VectorGridPrototype = L.GridLayer.extend({
         setTimeout(() => draw(0, this.boring), 0);
         return tile;
     }
-});
+}
 
-module.exports.Create = function (MaxZoom, Offset) {
-        var u = new VectorGridPrototype({updateWhenZooming: false, noWrap: true});
-        var size = u.getTileSize();
-        u.sources = [];
-        u.max_zoom = MaxZoom;
-        u.offset = Offset;
-        u.grid_x_size = Math.pow(2, MaxZoom);
-        u.grid_x_width = size.x / u.grid_x_size;
-        u.grid_y_size = Math.pow(2, MaxZoom);
-        u.grid_y_height = size.y / u.grid_y_size;
-        u.Offset = Offset;
-        var canvas = L.DomUtil.create('canvas', 'leaflet-tile');
-        ctx = canvas.getContext('2d');
-        u.Offset = Offset;
-        u.addText = (text, original_text, control, x, y, zoomMin, zoomMax, color, scale) => {
-            controlToFont(control, ctx, u.boring);
-            var size = ctx.measureText(u.boring ? original_text : text);
-            u.sources.push(
-                {
-                    size: {
-                        width: (size.actualBoundingBoxRight - size.actualBoundingBoxLeft) / u.grid_x_size,
-                        height: (size.actualBoundingBoxAscent + size.actualBoundingBoxDescent) / u.grid_y_size
-                    },
-                    text: text,
-                    original_text: original_text,
-                    x: x + Offset[0],
-                    y: -(y + Offset[1]) + 256,
-                    control: control,
-                    zoomMin: zoomMin,
-                    zoomMax: zoomMax,
-                    color: color,
-                    scale: scale == null ? 1 : scale
-                });
-        };
-        const loaded_events = [];
-        const unloaded_events = [];
-        u.when = function (event_name, event_action) {
-            switch (event_name) {
-                case 'loaded':
-                    loaded_events.push(event_action);
-                    break;
-                case 'unloaded':
-                    unloaded_events.push(event_action);
-                    break;
-            }
-        };
-        u.on('loading', () => {
-            for (let i of unloaded_events) i();
-        });
-        u.on('load', () => {
-            for (let i of loaded_events) i();
-        });
-        return u;
+export function Create(MaxZoom, Offset) {
+    const u = new VectorGridPrototype({updateWhenZooming: false, noWrap: true});
+    const size = u.getTileSize();
+    u.sources = [];
+    u.max_zoom = MaxZoom;
+    u.offset = Offset;
+    u.grid_x_size = Math.pow(2, MaxZoom);
+    u.grid_x_width = size.x / u.grid_x_size;
+    u.grid_y_size = Math.pow(2, MaxZoom);
+    u.grid_y_height = size.y / u.grid_y_size;
+    u.Offset = Offset;
+    const canvas = L.DomUtil.create('canvas', 'leaflet-tile');
+    ctx = canvas.getContext('2d');
+    u.Offset = Offset;
+    u.addText = (text, original_text, control, x, y, zoomMin, zoomMax, color, scale) => {
+        controlToFont(control, ctx, u.boring);
+        const size = ctx.measureText(u.boring ? original_text : text);
+        u.sources.push(
+            {
+                size: {
+                    width: (size.actualBoundingBoxRight - size.actualBoundingBoxLeft) / u.grid_x_size,
+                    height: (size.actualBoundingBoxAscent + size.actualBoundingBoxDescent) / u.grid_y_size
+                },
+                text: text,
+                original_text: original_text,
+                x: x + Offset[0],
+                y: -(y + Offset[1]) + 256,
+                control: control,
+                zoomMin: zoomMin,
+                zoomMax: zoomMax,
+                color: color,
+                scale: scale == null ? 1 : scale
+            });
     };
+    const loaded_events = [];
+    const unloaded_events = [];
+    u.when = function (event_name, event_action) {
+        switch (event_name) {
+            case 'loaded':
+                loaded_events.push(event_action);
+                break;
+            case 'unloaded':
+                unloaded_events.push(event_action);
+                break;
+        }
+    };
+    u.on('loading', () => {
+        for (let i of unloaded_events) i();
+    });
+    u.on('load', () => {
+        for (let i of loaded_events) i();
+    });
+    return u;
+}
